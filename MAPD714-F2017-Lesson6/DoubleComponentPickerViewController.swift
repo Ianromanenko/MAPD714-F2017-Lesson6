b26d28aacaf3669ca02869882be77cc1f88ae86b
yan <ianromanenko@gmail.com>
2017-10-11 21:36:24 -0400

    added double picker with problems

--- MAPD714-F2017-Lesson6/DoubleComponentPickerViewController.swift
+++ MAPD714-F2017-Lesson6/DoubleComponentPickerViewController.swift
@@ -1,10 +1,48 @@
 import UIKit
 
-class DoubleComponentPickerViewController: UIViewController {
+class DoubleComponentPickerViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    
 
+    @IBOutlet weak var DoublePickerView: UIPickerView!
+    private let fillingComponent = 0
+    private let breadComponent = 1
+    private let fillingTypes = ["Turkey", "Ham", "Steak"]
+    private let breadTypes = ["111", "222", "333"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+    @IBAction func OnButtonPressed(_ sender: UIButton) {
+        let fillingRow = doublePicker.selectedRow(inComponent: fillingComponent)
+        let breadRow = doublePicker.selectedRow(inComponent: breadComponent)
+        
+        let filling = fillingTypes[fillingRow]
+        let bread = breadTypes[breadRow]
+        let message = "You \(filling) on \(bread) bread will beright up"
+        
+        let alert = UIAlertController(title: "Thank u for order", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Great", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == breadComponent{
+            return breadTypes.count
+        } else{
+            return fillingTypes.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return ""
+    }
 }
