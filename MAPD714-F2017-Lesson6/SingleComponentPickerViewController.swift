7213ec457a85400ec8eafe6dd64f0a29d4dc6fb2
yan <ianromanenko@gmail.com>
2017-10-11 21:15:08 -0400

    single added

--- MAPD714-F2017-Lesson6/SingleComponentPickerViewController.swift
+++ MAPD714-F2017-Lesson6/SingleComponentPickerViewController.swift
@@ -1,10 +1,53 @@
 import UIKit
 
-class SingleComponentPickerViewController: UIViewController {
+class SingleComponentPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    
+    @IBOutlet weak var singlePicker: UIPickerView!
+    
+    private let characterNames = ["Luke", "Leia", "Han", "Chewbacca", "Arthoo", "Threepio", "Lando"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-
+    
+    @IBAction func OnButtonPressed(_ sender: UIButton) {
+        
+        let row = singlePicker.selectedRow(inComponent: 0)
+        let selected = characterNames[row]
+        let title = "You selected \(selected)!"
+        
+        let alert = UIAlertController(
+            title: title,
+            message: "Thank you for choosing",
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(
+            title: "You are welcome",
+            style: .default,
+            handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+    //Data sourse
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return characterNames.count
+    }
+    
+    //Delegate
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return characterNames[row]
+    }
+    
 }
